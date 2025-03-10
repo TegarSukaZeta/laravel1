2428ff1800b98809d4bd40d3bb03ba8b10de7c75
TegarSukaZeta <nataliasuparmi27@gmail.com>
2025-03-10 12:11:53 +0700

    Cihuy

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil Pengguna</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-4">
+            <div class="card">
+        <div class="card-header">
+            <h3>Profil Saya</h3>
+        </div>
+        <div class="card-body">
+            
+            <img src="Rusdi_Omaygot.png" alt="Profile Picture" width="100">
+
+            
+            <h4>Nama: Imanuel Tegar NIA</h4>
+            <p>Email: imanueltegarn@gmail.com</p>
+            <p>Bio: Sayung to Mekkah</p>
+
+            
+                            <h5>Skills:</h5>
+                <ul>
+                    
+                                            <li>Player xp-laner yang jago</li>
+                                            <li>Cuan Maksimal</li>
+                                            <li>Masak</li>
+                                            <li>Jago C--</li>
+                                            <li>Suka menabung</li>
+                                            <li>Baik hati</li>
+                                            <li>Enggak red flag</li>
+                                            <li>Suka mommy cantik</li>
+                                            <li>Pendekar Ngawi</li>
+                                    </ul>
+                    </div>
+    </div>
+    </div>
+</body>
+</html>
